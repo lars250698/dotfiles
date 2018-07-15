b47a59a5e21d19b9a6906dae6d6b47237f65d954
Lars Eppinger <lars.eppinger@posteo.de>
2018-07-15 16:28:55 +0200

    fixed bug that caused polybar to not start

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,6 +2,8 @@
 
 killall -q polybar
 
+while pgrep -x wal > /dev/null; do sleep 1; done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar1 &
