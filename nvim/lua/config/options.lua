cad2c6217d49725de594571e8d2242ee1197bb7a
Lars Eppinger <lars@eppinger.dev>
2024-10-15 01:55:04 +0200

    keybindings, improvements

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.scrolloff = 12
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
